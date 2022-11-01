72f7a79a6b767f25f009e0534964a96ba2e842d8
liornoy <ironoy@gmail.com>
2022-11-01 16:34:49 +0200

    Fix models.py file
    Fixed flake8 related errors, and added max_length
    to CharField.
    Also on the Review model, changed to models.Model

--- reservations/models.py
+++ reservations/models.py
@@ -1,24 +1,27 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+
 class Room (models.Model):
     number = models.IntegerField()
     floor = models.IntegerField()
     price_per_night = models.FloatField()
     details = models.TextField()
 
+
 class Guest (models.Model):
-    name = models.CharField()
-    email = models.CharField()
+    name = models.CharField(max_length = 120)
+    email = models.CharField(max_length = 120)
+
 
 class Reservation(models.Model):
     guest = models.ForeignKey(Guest, on_delete=models.PROTECT)
     room = models.ForeignKey(Room, on_delete=models.PROTECT)
     date = models.DateField()
 
-class Review(models.model):
+
+class Review(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(0),
-                                       MaxValueValidator(10)])
+                                 MaxValueValidator(10)])
     guest = models.ForeignKey(Guest, on_delete=models.CASCADE)
     content = models.TextField
-    
