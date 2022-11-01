1eba08f8758deab7b064dd59d12d8d00e2810369
liornoy <ironoy@gmail.com>
2022-11-01 14:32:23 +0200

    Add data model class to reservation app
    This commit modifies reservations/models.py and adds
    the following data model classes:
    Room, Guest, Reservation, Review

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Room (models.Model):
+    number = models.IntegerField()
+    floor = models.IntegerField()
+    price_per_night = models.FloatField()
+    details = models.TextField()
+
+class Guest (models.Model):
+    name = models.CharField()
+    email = models.CharField()
+
+class Reservation(models.Model):
+    guest = models.ForeignKey(Guest, on_delete=models.PROTECT)
+    room = models.ForeignKey(Room, on_delete=models.PROTECT)
+    date = models.DateField()
+
+class Review(models.model):
+    rating = models.IntegerField(validators=[MinValueValidator(0),
+                                       MaxValueValidator(10)])
+    guest = models.ForeignKey(Guest, on_delete=models.CASCADE)
+    content = models.TextField
+    
